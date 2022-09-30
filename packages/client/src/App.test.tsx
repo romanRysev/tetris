67ee1b44e958a339d87d0f5330eb63ae6be8542e
Kirill <kirill.kirill2@yandex.ru>
2022-09-30 12:45:13 +0500

    hotfix linter

--- packages/client/src/App.test.tsx
+++ packages/client/src/App.test.tsx
@@ -1,14 +1,14 @@
-import App from './App'
-import { render, screen } from '@testing-library/react'
+import App from './App';
+import { render, screen } from '@testing-library/react';
 
-const appContent = 'Вот тут будет жить ваше приложение :)'
+const appContent = 'Вот тут будет жить ваше приложение :)';
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-global.fetch = jest.fn(() =>
-  Promise.resolve({ json: () => Promise.resolve('hey') })
-)
+global.fetch = jest.fn(() => Promise.resolve({ json: () => Promise.resolve('hey') }));
 
 test('Example test', async () => {
-  render(<App />)
-  expect(screen.getByText(appContent)).toBeDefined()
-})
+  // eslint-disable-next-line react/react-in-jsx-scope
+  render(<App />);
+  expect(screen.getByText(appContent)).toBeDefined();
+});
