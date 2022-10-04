e48c5684d1bf25ceaa691e59d8296ba02b875fca
Roman Rysev <Roman.Rysev@sitronics-kt.ru>
2022-10-04 21:58:38 +0300

    компонент Input

---
+++ packages/client/src/components/Input/Input.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import './index.scss';
+
+interface Props {
+  className?: string;
+  value?: string | undefined;
+  disabled?: boolean;
+  type?: React.HTMLInputTypeAttribute;
+  placeholder?: string;
+  label?: string;
+  errorText?: string;
+  onChange?: React.ChangeEventHandler<HTMLInputElement>;
+  onBlur?: React.FocusEventHandler<HTMLInputElement>;
+}
+
+const input: React.FC<Props> = ({
+  className = '',
+  disabled,
+  value,
+  type,
+  placeholder,
+  label,
+  errorText,
+  onChange,
+  onBlur,
+}) => {
+  return (
+    <div className="input-container">
+      <label className="input__label">
+        <span className="input__title">{label}</span>
+        <input
+          className={`input input_theme_light ${className}`}
+          value={value}
+          type={type}
+          placeholder={placeholder}
+          disabled={disabled}
+          onChange={onChange}
+          onBlur={onBlur}
+        />
+        <span className="input__error">{errorText}</span>
+      </label>
+    </div>
+  );
+};
+
+export const Input = input;
