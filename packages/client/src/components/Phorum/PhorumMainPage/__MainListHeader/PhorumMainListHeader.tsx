84bd71ac944970ebe47c9acbd1cd0c5e1d767b65
Veronika Zayarnaya <veronika.zayarnaya@yandex.ru>
2022-10-18 15:15:17 +0300

    fix по результатам ревью

--- packages/client/src/components/Phorum/PhorumMainPage/__MainListHeader/PhorumMainListHeader.tsx
+++ packages/client/src/components/Phorum/PhorumMainPage/__MainListHeader/PhorumMainListHeader.tsx
@@ -1,22 +1,12 @@
 import React, { FC } from 'react';
 import './PhorumMainListHeader.scss';
 
-interface MainListHeaderProps {
-  threadHeader?: string;
-  repliesHeader?: string;
-  lastReplyHeader?: string;
-}
-
-export const MainListHeader: FC<MainListHeaderProps> = ({
-  threadHeader = 'Тема',
-  repliesHeader = 'Ответов',
-  lastReplyHeader = 'Последний ответ',
-}) => {
+export const MainListHeader: FC = () => {
   return (
     <div className="thread-list__head">
-      <div className="thread-list__thread_header">{threadHeader}</div>
-      <div className="thread-list__replies_header">{repliesHeader}</div>
-      <div className="thread-list__last-reply_header">{lastReplyHeader}</div>
+      <div className="thread-list__thread_header">Тема</div>
+      <div className="thread-list__replies_header">Ответов</div>
+      <div className="thread-list__last-reply_header">Последний ответ</div>
     </div>
   );
 };
