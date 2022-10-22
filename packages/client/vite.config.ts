b558b9b68c0efbedf968ef598601b335dd5dee86
Veronika Zayarnaya <veronika.zayarnaya@yandex.ru>
2022-10-22 12:54:28 +0300

    added vite-plugin-pwa

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import dotenv from 'dotenv';
+import { VitePWA } from 'vite-plugin-pwa';
 dotenv.config();
 
 // https://vitejs.dev/config/
@@ -11,5 +12,14 @@ export default defineConfig({
   define: {
     __SERVER_PORT__: process.env.SERVER_PORT,
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    // eslint-disable-next-line new-cap
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
 });
