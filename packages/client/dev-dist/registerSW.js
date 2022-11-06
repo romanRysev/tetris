134dc923788757e64d6e6222b279d71833019fc7
Veronika Zayarnaya <veronika.zayarnaya@yandex.ru>
2022-11-06 11:51:14 +0300

    вернула как было

--- packages/client/dev-dist/registerSW.js
+++ packages/client/dev-dist/registerSW.js
@@ -1,15 +1,2 @@
-// if ('serviceWorker' in navigator)
-//   navigator.serviceWorker.register('/dev-sw.js?dev-sw', { scope: '/', type: 'classic' });
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker
-      .register('/dev-sw.js?dev-sw', { scope: '/', type: 'classic' })
-      .then((registration) => {
-        console.log('ServiceWorker registration successful with  scope: ', registration.scope);
-      })
-      .catch((error) => {
-        console.log('ServiceWorker registration failed: ', error);
-      });
-  });
-}
+if ('serviceWorker' in navigator)
+  navigator.serviceWorker.register('/dev-sw.js?dev-sw', { scope: '/', type: 'classic' });
