f99dbe1589acfa3a9c31b98118f005e87a3d02fb
Veronika Zayarnaya <veronika.zayarnaya@yandex.ru>
2022-11-07 14:36:19 +0300

    delete commented code

--- packages/client/src/components/LeaderItem/LeaderItem.tsx
+++ packages/client/src/components/LeaderItem/LeaderItem.tsx
@@ -1,6 +1,5 @@
 import classNames from 'classnames';
 import React, { FC } from 'react';
-// import { defaulAvatar, filePrefix } from '../../utils/constants';
 import { UserAvatar } from '../UserAvatar/UserAvatar';
 import './LeaderItem.scss';
 
