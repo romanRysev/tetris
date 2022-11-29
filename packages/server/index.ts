c388560c4d95f13e745f21aef89daafb84251dea
Kirill <kirill.kirill2@yandex.ru>
2022-11-29 14:51:05 +0500

    ssr

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,11 +1,19 @@
 import dotenv from 'dotenv'
 import cors from 'cors'
 import express from 'express'
-import { startApp } from './app/config/db.config'
+import * as path from 'path'
+import * as fs from 'fs'
 import bodyParser from 'body-parser'
+
+import { startApp } from './app/config/db.config'
+
 import userRouter from './routes/userRoutes'
 import themeRouter from './routes/themeRoutes'
 import forumRouter from './routes/forumRoutes'
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import { render } from '../client/dist/ssr/entry-server.cjs'
 
 dotenv.config()
 
@@ -15,44 +23,22 @@ const port = Number(process.env.SERVER_PORT) || 3001
 
 app.use(bodyParser.json())
 
-app.use('/api/user', userRouter);
-app.use('/api/theme', themeRouter);
-app.use('/api/forum', forumRouter);
+app.use('/api/user', userRouter)
+app.use('/api/theme', themeRouter)
+app.use('/api/forum', forumRouter)
 
 startApp()
 
 app.get('/', (_, res) => {
-  res.json('👋 Howdy from the server :) ')
+  const result = render()
+  const template = path.resolve(__dirname, '../client/dist/client/index.html')
+  const htmlString = fs.readFileSync(template, 'utf-8')
+  const newString = htmlString.replace('<!--ssr-outlet-->', result)
+  res.send(newString)
 })
+
+app.use(express.static(path.resolve(__dirname, '../client/dist/client')))
 
 app.listen(port, () => {
   console.log(`  ➜ 🎸 Server is listening on port: ${port}`)
 })
-
-
-
-//пример запроса
-// fetch('/api/user', {
-//   method: 'POST',
-//   headers: {
-//     'Content-Type': 'application/json'
-//   },
-//   body: JSON.stringify({
-//     firstName: "Mort",
-//     secondName: "Emperorsson",
-//     id: "234",
-//     displayName: "",
-//     login: "mort",
-//     email: "mort@morty.mort",
-//     phone: "0987654321",
-//     avatar: "",
-//   })
-// })
-
-//пример запроса на findALl
-// fetch('/api/user?userID=26', {
-//   method: 'GET',
-//   headers: {
-//     'Content-Type': 'application/x-www-form-urlencoded'
-//   },
-// })
