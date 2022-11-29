7798ad2539d9753c94d98e27e44453c3f72b8db8
Kirill <kirill.kirill2@yandex.ru>
2022-11-29 18:02:33 +0500

    hotfix

--- packages/client/src/entry-server.tsx
+++ packages/client/src/entry-server.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
 import { renderToString } from 'react-dom/server';
 import App from './App';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
-export const render = () => renderToString(<App />);
+export const render = () =>
+  renderToString(
+    <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>,
+  );
